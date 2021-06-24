5ab396feb9279bd9c02ccd5e1cae6fca988b80bf
Jaine <jaine_saconi@hotmail.com>
2021-06-24 11:29:38 -0300

    Arquitetura do projeto

---
+++ WEB2/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContentComponent } from './components/content/content.component';
+import { ProjectComponent } from './components/project/project.component';
+import { TeamComponent } from './components/team/team.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuBarComponent,
+    HomeComponent,
+    ContentComponent,
+    ProjectComponent,
+    TeamComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
