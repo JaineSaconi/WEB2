0d1fa4243a5ffeab2eb7c1d12a59141138a86140
Jaine <jaine_saconi@hotmail.com>
2021-06-28 14:27:05 -0300

    navbar

--- WEB2/src/app/app-routing.module.ts
+++ WEB2/src/app/app-routing.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
+import { ContentComponent } from './components/content/content.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProjectComponent } from './components/project/project.component';
+import { TeamComponent } from './components/team/team.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  }
+  },
+  {
+    path: 'content',
+    component: ContentComponent
+  },
+  {
+    path: 'project',
+    component: ProjectComponent
+  },
+  {
+    path: 'team',
+    component: TeamComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
 ];
 
 @NgModule({
