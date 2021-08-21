6d64ce485eeac998a6040279f960004b1711a9b9
Jaine <jaine_saconi@hotmail.com>
2021-08-21 14:01:16 -0300

    ajustes

--- node_express_mongo-login_0/src/app/models/user.js
+++ node_express_mongo-login_0/src/app/models/user.js
@@ -34,6 +34,10 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: false,
     },
+    qtdQuestoesCertas:{
+        type: Number,
+        required: false
+    },
     createdAt: {
         type: Date,
         default: Date.now,
